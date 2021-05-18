14cc74508f718e6f9c71464d4830ad9ea3f4f255
Ranjeet Harishchandre <ranjeetharishchandre@gmail.com>
2021-05-18 22:11:49 +0530

    Start of project at 39.07 18/05/21 1011PM

--- components/Header.js
+++ components/Header.js
@@ -18,7 +18,6 @@ import {
 function Header() {
   return (
     <div>
-      
       {/* Left */}
       <div className="flex items-center">
         <Image
@@ -27,14 +26,19 @@ function Header() {
           height={40}
           layout="fixed"
         />
-        <div className="flex ml-2 items-center">
-          <SearchIcon className="h-6"/>
-          <input type="text" name="" id="" placeholder="Search Facebook" />
+        <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+          <SearchIcon className="h-6 text-gray-600" />
+          <input
+            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500"
+            type="text"
+            name=""
+            id=""
+            placeholder="Search Facebook"
+          />
         </div>
       </div>
 
       {/* Center */}
-
 
       {/* Right */}
     </div>
--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,8 @@ export default function Home() {
       <Head>
         <title>Facebook</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Roboto&display=swap" rel="stylesheet"/>
       </Head>
 
      <Header/>
